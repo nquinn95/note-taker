ccf9bb6b9597fb47fd238db90078666628e182b4
nick quinn <nick.quinn@stadium1.com>
2022-05-31 10:21:12 -0400

    added to server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+
+//routes
+const apiRoute = require("./routes/apiRoute");
+const htmlRoute = require("./routes/htmlRoute");
+
+const app = express();
+const PORT = process.env.PORT || 3001; 
+
+
+app.use(express().json);
+app.use("/api", apiRoute);
+app.use("", htmlRoute);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
